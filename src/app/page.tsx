dae0ea5006e973847a55e05cc26233fed7b2f657
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-06 10:30:51 +0900

    #91.3 Bài tập Design App Bar (Part 3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import AppHeader from "@/components/header/app.header";
+
 export default function HomePage() {
-  return <div className=" text-fuchsia-400">home page</div>;
+  return (
+    <div className=" text-fuchsia-400">
+      <AppHeader />
+    </div>
+  );
 }
