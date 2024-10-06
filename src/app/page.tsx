a9632ba293fc9923a3e307e8c8428a8d50022862
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-07 06:51:31 +0900

    #103.2 Fetch Wrapper (Typescript)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-import { sendRequestJS } from "/Users/lehau/Desktop/NextJs_Mui/src/components/utils/old.api.js";
+import { sendRequest } from "@/components/utils/api";
 export default async function HomePage() {
-  const res = await sendRequestJS({
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
-    body: { category: "CHILL", limit: 1 },
+    body: { category: "CHILL", limit: 2 },
   });
-
-  console.log(res);
   return (
     <>
       <Container>
