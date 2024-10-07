5f3643f84ab98ceb5b52e282c7501d84f073c2a2
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-08 07:02:36 +0900

    #114. Handle Event với useCallback

--- src/components/track/wawe.track.tsx
+++ src/components/track/wawe.track.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { useRef } from "react";
 import { useSearchParams } from "next/navigation";
 import { useWavesurfer } from "../utils/customHooks";
@@ -8,6 +8,7 @@ const WaveTrack = () => {
   const searchParams = useSearchParams();
   const fileName = searchParams.get("audio");
   const containerRef = useRef<HTMLInputElement>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   const optionsMemo = useMemo(() => {
     return {
@@ -19,7 +20,37 @@ const WaveTrack = () => {
 
   const wavesurfer = useWavesurfer(containerRef, optionsMemo);
 
-  return <div ref={containerRef}>WaveTrack</div>;
+  // Initialize wavesurfer when the container mounts
+  // or any of the props change
+  useEffect(() => {
+    if (!wavesurfer) return;
+    setIsPlaying(false);
+
+    const subscriptions = [
+      wavesurfer.on("play", () => setIsPlaying(true)),
+      wavesurfer.on("pause", () => setIsPlaying(false)),
+    ];
+
+    return () => {
+      subscriptions.forEach((unsub) => unsub());
+    };
+  }, [wavesurfer]);
+
+  // On play button click
+  const onPlayClick = useCallback(() => {
+    if (wavesurfer) {
+      wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
+    }
+  }, [wavesurfer]);
+
+  return (
+    <div>
+      <div ref={containerRef}>WaveTrack</div>
+      <button onClick={onPlayClick}>
+        {isPlaying === true ? "Pause" : "Play"}
+      </button>
+    </div>
+  );
 };
 
 export default WaveTrack;
