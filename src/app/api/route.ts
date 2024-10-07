f3f12e6facf1a72186eb7138e7fb67bc9f98cb61
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-08 05:44:22 +0900

    #111.3 Fetch Track From Remote Server (Backend)

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,9 @@
-export async function GET() {
-  console.log("check sever ");
-  return Response.json({ data: "leduyhau" });
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, response: NextResponse) {
+  const { searchParams } = new URL(request.url);
+  const fileName = searchParams.get("audio");
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/${fileName}`
+  );
 }
--- src/components/track/wawe.track.tsx
+++ src/components/track/wawe.track.tsx
@@ -2,16 +2,20 @@
 import React, { useEffect } from "react";
 import WaveSurfer from "wavesurfer.js";
 import { useRef } from "react";
+import { useSearchParams } from "next/navigation";
 
 const WaveTrack = () => {
+  const searchParams = useSearchParams();
+  const fileName = searchParams.get("audio");
   const containerRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     if (containerRef.current) {
       WaveSurfer.create({
         container: containerRef.current,
         waveColor: "rgb(200, 0, 200)",
         progressColor: "rgb(100, 0, 100)",
-        url: "http://localhost:8000/tracks/CHILL.mp3",
+        url: `/api?audio=${fileName}`,
       });
     }
   }, []);
