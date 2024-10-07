766358163a96860d9013cfe55a1631aec01ca6a6
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-07 10:32:13 +0900

    #105. Dynamic Route

---
+++ src/app/track/[slug]/page.tsx
@@ -0,0 +1,6 @@
+const DetailTrackPage = ({ params }: { params: { slug: string } }) => {
+  console.log(params.slug);
+  return <div>My Post: {params.slug}</div>;
+};
+
+export default DetailTrackPage;
