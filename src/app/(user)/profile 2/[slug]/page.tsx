6f881cd002fea1901c40b7a6131dbfb1acba45db
duyhaule2001 <duyhaule2001@gmail.com>
2024-10-17 06:16:18 +0900

    #155. Bài tập Render List Tracks Profile

---
+++ src/app/(user)/profile 2/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return <>profile slug</>;
+};
+
+export default ProfilePage;
--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,14 +1,28 @@
+import ProfilePage from "@/components/header/profile.track";
 import { sendRequest } from "@/components/utils/api";
+import { Container, Grid } from "@mui/material";
 
 const Profile = async ({ params }: { params: { slug: string } }) => {
-  const track = await sendRequest<IBackendRes<ITrackTop[]>>({
+  const track = await sendRequest<IBackendRes<IModelPaginate<ITrackTop>>>({
     url: "http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10",
     method: "POST",
     body: { id: params.slug },
   });
 
-  console.log("check track", track);
-  return <div>abcd {params.slug}</div>;
+  const data = track?.data?.result ?? [];
+  return (
+    <Container sx={{ my: 5 }}>
+      <Grid container spacing={5}>
+        {data.map((item: ITrackTop, index: number) => {
+          return (
+            <Grid item xs={12} md={6} key={index}>
+              <ProfilePage data={item} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Profile;
